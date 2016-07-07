40e5f33b682f1efec426fae8872ffca6cd1f8f3c
destroyergm <stefan_vg@hotmail.com>
2016-07-07 19:40:38 +0200

    Add Game object

--- assets/game.js
+++ assets/game.js
@@ -1,3 +1,30 @@
+var Cell = function(val) {
+	/*
+		cell - empty cell
+		snake - snake parts
+		food - food part
+		wall - wall(collision)
+	*/
+	this.val = val;
+}
+
+var Game = function(size) {
+	this.size = size;
+	this.grid = new Array(size);
+	for(var i=0; i < size; i++) {
+		grid[i] = new Array(size);
+	}
+}
+
+Game.prototype.render = function() {
+	for(var i=0; i < size; i++) {
+		for(var j=0; j < size; j++) {
+
+		}
+	}
+}
+
+
 $(document).ready(function() {
-	alert("jQuery loaded");
+	
 });
