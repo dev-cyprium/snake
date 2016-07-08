9dcbff807aa8e41a8b9e3bc50db87bcdd40184b1
destroyergm <stefan_vg@hotmail.com>
2016-07-08 12:51:32 +0200

    Add snake movement

--- assets/game.js
+++ assets/game.js
@@ -8,16 +8,31 @@ var Cell = function(val) {
 	this.state = val;
 }
 
+var Vector2 = function(x,y) {
+	this.x = x;
+	this.y = y;
+}
+
+Vector2.prototype.add = function(vec2) {
+	this.x += vec2.x;
+	this.y += vec2.y;
+}
+
 var Game = function(size) {
 	this.size = size;
 	this.grid = new Array(size);
+	this.snake = new Snake(size/2, size/2);
 	for(var i=0; i < size; i++) {
 		this.grid[i] = new Array(size);
 	}
 
 	for(var i=0; i < size; i++) {
 		for(var j=0; j < size; j++) {
-			this.grid[i][j] = new Cell('empty');
+			if(i == this.snake.x && j == this.snake.y) {
+				this.grid[i][j] = new Cell('snake');
+			} else {
+				this.grid[i][j] = new Cell('empty');
+			}
 		}
 	}
 }
@@ -36,17 +51,47 @@ Game.prototype.create = function() {
 	}
 }
 
+Game.prototype.update = function() {
+	this.snake.position.add(this.snake.direction);
+	for(var i=0; i < this.size; i++) {
+		for(var j=0; j < this.size; j++) {
+			if(i == this.snake.position.x && j == this.snake.position.y) {
+				this.grid[i][j].state = "snake";
+			} else {
+				this.grid[i][j].state = "empty";
+			}
+		}
+	}
+}
+
 Game.prototype.render = function() {
 	for(var i=0; i < this.size; i++) {
 		for(var j=0; j < this.size; j++) {
 			state = this.grid[i][j].state;
-			console.log("cell-"+i+"-"+j+" has state: " + state);
+			var color = 'white';
+			switch(state) {
+				case 'snake':
+					color = "black";
+					break;
+			}
+			$("#cell-"+i+"-"+j).css("background-color",color);
 		}
 	}
+}
+
+var Snake = function(x,y) {
+	this.position = new Vector2(x,y);
+	this.cells = [];
+	this.cells.push([x,y]);
+	this.direction = new Vector2(0,1);
 }
 
 $(document).ready(function() {
 	var game = new Game(20);
 	game.create();
-	game.render();
+	
+	var loop = setInterval(function() {
+		game.update();
+		game.render();
+	}, 250);
 });
